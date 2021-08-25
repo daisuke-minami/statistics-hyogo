73cb4e7d8f024cd0e0f0f3f05e71b8b9be1d3b9f
daisuke-minami <daisuke.minami.1979@gmail.com>
2021-08-25 18:27:28 +0900

    test

--- ui-test/ogp_screenshot.py
+++ ui-test/ogp_screenshot.py
@@ -18,58 +18,26 @@ routes = json.load(j)
 if not os.path.exists("ogp"):
     os.mkdir("ogp")
 
+# #ChromeDriverの設定
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+options.add_argument("--hide-scrollbars")
+driver = webdriver.Chrome(options=options)
+
 #ChromeDriverの設定
-chromePath = os.path.join(root_dir, 'node_modules/chromedriver/bin/chromedriver')
-driver = webdriver.Chrome(executable_path=chromePath)
-driver.set_window_size(959, 500)
+# chromePath = os.path.join(root_dir, 'node_modules/chromedriver/bin/chromedriver')
+# driver = webdriver.Chrome(executable_path=chromePath)
+# driver.set_window_size(959, 500)
 
 
 for path in routes:
-    # driver.set_window_size(959, 500)
-    # driver.get(
-    #     "http://localhost:3000{}?ogp=true".format(path)
-
-    # url = ("http://localhost:3000/{}?ogp=true".format(path))
-    # print(url)
-
-    # FILENAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
-    # print(FILENAME)
-
+    driver.set_window_size(959, 500)
     driver.get(
         ("http://localhost:3000/{}?ogp=true".format(path))
         )
     driver.save_screenshot(
             "ogp/{}.png".format(path)
         )
-    # driver.quit()
-    # driver.get(url)
-    # driver.save_screenshot(FILENAME)
-
-    #  driver.save_screenshot(
-#             "ogp/{}.png".format(
-#                 path if lang == "ja" else "{}/{}".format(lang, path)
-#             )
-#         )
-    
-
-
-#ChromeDriverの設定
-# chromePath = os.path.join(root_dir, 'node_modules/chromedriver/bin/chromedriver')
-# driver = webdriver.Chrome(executable_path=chromePath)
-# url = 'https://statistics-hyogo.com'
-# driver.get(url)
-# driver.set_window_size(959, 500)
-# driver.save_screenshot(FILENAME)
-
-# Chromeを閉じる
-# driver.quit()
-
-# #ChromeDriverの設定
-# options = webdriver.ChromeOptions()
-# options.add_argument("--headless")
-# options.add_argument("--hide-scrollbars")
-# driver = webdriver.Chrome(options=options)
-
 
 
 # #OGP画像の生成（都道府県）
