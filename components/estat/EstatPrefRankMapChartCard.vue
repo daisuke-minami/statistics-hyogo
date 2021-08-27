04a695b9574838aa20f3d748564868a3141e5a02
DAISUKE MINAMI <79508597+daisuke-minami@users.noreply.github.com>
2021-08-27 10:23:31 +0900

    Update EstatPrefRankMapChartCard.vue

--- components/estat/EstatPrefRankMapChartCard.vue
+++ components/estat/EstatPrefRankMapChartCard.vue
@@ -9,19 +9,19 @@
               {{ title }}
             </h4>
 
-      <v-radio-group
-        v-model="row"
-        row
-      >
-        <v-radio
-          label="Option 1"
-          value="radio-1"
-        ></v-radio>
-        <v-radio
-          label="Option 2"
-          value="radio-2"
-        ></v-radio>
-      </v-radio-group>
+            <v-radio-group
+              v-model="row"
+              row
+            >
+              <v-radio
+                label="Option 1"
+                value="radio-1"
+              ></v-radio>
+              <v-radio
+                label="Option 2"
+                value="radio-2"
+              ></v-radio>
+            </v-radio-group>
 
             <v-select
               v-model="targetYear"
