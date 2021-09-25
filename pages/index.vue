14b89d89850d2e763b13637b10607299b77e4156
daisuke-minami <daisuke.minami.1979@gmail.com>
2021-09-25 22:22:04 +0900

    不要なコンポーネントを削除

--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,26 @@
 <template>
   <div>
-    <v-tabs v-model="tab" hide-slider>
-      <v-tab
-        v-for="(item, i) in items"
-        :key="i"
-        v-ripple="false"
-        :href="`#tab-${i}`"
-        @click="change"
-      >
-        {{ item.label }}
-      </v-tab>
-      <v-tabs-items v-model="tab" touchless>
-        <v-tab-item v-for="(item, i) in items" :key="i" :value="`tab-${i}`">
-          <component :is="item.component" />
-        </v-tab-item>
-      </v-tabs-items>
-    </v-tabs>
+    <card-row class="DataBlock">
+      <!-- 県民局 -->
+      <hyogo-kenmin-kyoku-card />
+      <!-- 市区町村合併 -->
+      <!-- <hyogo-city-card :pref-code="prefCode" :pref-name="prefName" /> -->
+    </card-row>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { mapGetters } from 'vuex'
 
-import CardsIndexPref from '@/components/container/index/CardsIndexPref.vue'
-// import CardsIndexCity from '@/components/index/CardsIndexCity.vue'
-import { EventBus, TOGGLE_EVENT } from '@/utils/tab-event-bus.ts'
 export default Vue.extend({
-  components: {
-    CardsIndexPref,
-    // CardsIndexCity,
-  },
-  data() {
-    return {
-      tab: null,
-      items: [
-        { label: '兵庫県の統計', component: CardsIndexPref },
-        // { label: '市区町村の統計', component: CardsIndexCity },
-      ],
-    }
-  },
-  methods: {
-    change() {
-      EventBus.$emit(TOGGLE_EVENT)
+  computed: {
+    ...mapGetters('prefList', ['getSelectedPrefCode', 'getPrefName']),
+    prefCode() {
+      return this.getSelectedPrefCode
+    },
+    prefName() {
+      return this.getPrefName(this.prefCode)
     },
   },
 })
