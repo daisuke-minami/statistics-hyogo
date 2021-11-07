f6dc87cc89bc332d397f87c7ef496b291fd6da18
daisuke-minami <daisuke.minami.1979@gmail.com>
2021-11-07 22:14:52 +0900

    test

--- static/pagecontents/createContents.py
+++ static/pagecontents/createContents.py
@@ -5,7 +5,7 @@ import estat
 import resas
 import pathlib
 
-#ルートディレクトリの設定
+# ルートディレクトリの設定
 root_dir = pathlib.Path(__file__).parent.parent.parent
 
 # statisticsClassを取得
@@ -15,43 +15,69 @@ with open(c) as j:
     setting = json.load(j)
     statisticsClass = [d.get('id') for d in setting['statisticsClass']]
 
-#chartClassの定義
-chartClass =['prefecture','city']
+# chartClassの定義
+chartClass = ['prefecture', 'city']
 
-def saveEstatResponse(contents,statistics_type):
-    #estatAPIのパラメータ設定
-    params = estat.setEstatParams(contents['estatParams'],statistics_type)
 
-    #estatAPIのレスポンス取得
+def saveEstatResponse(contents):
+    params = {'appId': ESTAT_APPID}
+    params['statsDataId'] = contents['estatParams']['statsDataId']
+    if('cdCat01' in contents['estatParams']):
+        params['cdCat01'] = ','.join(
+            [d for d in contents['estatParams']['cdCat01']])
+
+    # prefecture
+    params['cdArea'] = '28000'
     res = estat.getEstatAPIResponse(params)
-
-    #ファイルに保存
     fileName = contents['titleId']
     downloadPath = os.path.join(down, fileName + '.json')
-    estat.saveJson(res,downloadPath,ensure_ascii=False)
+    estat.saveJson(res, downloadPath, ensure_ascii=False)
 
 
-#contentsの生成
+# contentsの生成
 for item in statisticsClass:
 
-    #ディレクトリを作成
+    # ディレクトリを作成
     directory = os.path.join(root_dir, 'static/pagecontents/'+item)
     pathlib.Path(directory).mkdir(exist_ok=True)
 
-    for type in chartClass:
-        #chartClassディレクトリを作成
-        down = os.path.join(directory, type)
-        pathlib.Path(down).mkdir(exist_ok=True)
+    params = {'appId': ESTAT_APPID}
 
-        #contentsの取得（都道府県）
-        path = os.path.join(root_dir, 'static/pagesetting')
-        j = open(path + '/' + item+'.json','r')
-        contentsList = json.load(j)[type]
+    # for type in chartClass:
+    #     #chartClassディレクトリを作成
+    #     down = os.path.join(directory, type)
+    #     pathlib.Path(down).mkdir(exist_ok=True)
 
-        for contents in contentsList:
-            if 'estatParams' in contents:
-                saveEstatResponse(contents,type)
-            # if 'resasUrl' in contents:
-            #     resas.saveResasResponse(contents,down,type)
+    #     #contentsの取得（都道府県）
+    #     path = os.path.join(root_dir, 'static/pagesetting')
+    #     j = open(path + '/' + item+'.json','r')
+    #     contentsList = json.load(j)[type]
 
+    #     for contents in contentsList:
+    #         if 'estatParams' in contents:
+    #             saveEstatResponse(contents,type)
+    #         # if 'resasUrl' in contents:
+    #         #     resas.saveResasResponse(contents,down,type)
 
+    # prefecture
+    down = os.path.join(directory, 'prefecture')
+    pathlib.Path(down).mkdir(exist_ok=True)
+
+    path = os.path.join(root_dir, 'static/pagesetting')
+    j = open(path + '/' + item+'.json', 'r')
+    contentsList = json.load(j)['prefecture']
+
+    for contents in contentsList:
+        if 'estatParams' in contents:
+            params['statsDataId'] = contents['estatParams']['statsDataId']
+            params['cdArea'] = '28000'
+            if('cdCat01' in contents['estatParams']):
+                params['cdCat01'] = ','.join(
+                    [d for d in contents['estatParams']['cdCat01']])
+
+            res = estat.getEstatAPIResponse(params)
+            fileName = contents['titleId']
+            downloadPath = os.path.join(down, fileName + '.json')
+            estat.saveJson(res, downloadPath, ensure_ascii=False)
+
+    # saveEstatResponse(contents, type)
