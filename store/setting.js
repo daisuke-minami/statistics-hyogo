288eeea10878fbd0a194742e1dc70c148015cacd
daisuke-minami <daisuke.minami.1979@gmail.com>
2021-11-18 08:27:20 +0900

    settingをstoreに格納

--- store/setting.js
+++ store/setting.js
@@ -6,6 +6,16 @@ import LaborWage from '@/data/pagesetting/laborwage.json'
 import Agriculture from '@/data/pagesetting/agriculture.json'
 import MiningIndustry from '@/data/pagesetting/miningindustry.json'
 import Commercial from '@/data/pagesetting/commercial.json'
+import Economy from '@/data/pagesetting/economy.json'
+import Construction from '@/data/pagesetting/construction.json'
+import Energy from '@/data/pagesetting/energy.json'
+import Tourism from '@/data/pagesetting/tourism.json'
+import EducationSports from '@/data/pagesetting/educationsports.json'
+import Administrativefinancial from '@/data/pagesetting/administrativefinancial.json'
+import Safetyenvironment from '@/data/pagesetting/safetyenvironment.json'
+import SocialSecurity from '@/data/pagesetting/socialsecurity.json'
+import International from '@/data/pagesetting/international.json'
+import Other from '@/data/pagesetting/other.json'
 
 const initialState = {
   statisticsClassList: [],
@@ -16,6 +26,16 @@ const initialState = {
   agriculture: Agriculture,
   miningindustry: MiningIndustry,
   commercial: Commercial,
+  economy: Economy,
+  construction: Construction,
+  energy: Energy,
+  tourism: Tourism,
+  educationsports: EducationSports,
+  administrativefinancial: Administrativefinancial,
+  safetyenvironment: Safetyenvironment,
+  socialsecurity: SocialSecurity,
+  international: International,
+  other: Other,
 }
 
 // ShallowCopyを避けるため、lodashのcloneDeepを用いる。
