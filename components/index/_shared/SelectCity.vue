32d30fda4ed0f80e97d3e09b7114c5d6cc3b7b93
daisuke-minami <daisuke.minami.1979@gmail.com>
2021-11-18 11:38:18 +0900

    SelectCityを修正

--- components/index/_shared/SelectCity.vue
+++ components/index/_shared/SelectCity.vue
@@ -2,18 +2,28 @@
   <div>
     <v-tabs dark background-color="teal darken-3" show-arrows>
       <v-tabs-slider color="teal lighten-3" />
-      <v-tab v-for="i in 30" :key="i" :href="'#tab-' + i"> Item {{ i }} </v-tab>
+      <v-tab
+        v-for="(item, i) in items"
+        :key="i"
+        :to="{ path: item.path }"
+        nuxt
+        exact
+        @click="change"
+      >
+        {{ item.label }}
+      </v-tab>
     </v-tabs>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, computed, useStore } from '@nuxtjs/composition-api'
+import { EventBus, TOGGLE_EVENT } from '@/utils/tab-event-bus'
 import { City } from '~/store/cityList'
 
 type Computed = {
   cityList: () => City[]
-  items: () => { label: string; link: string }
+  items: () => { label: string; path: string }
 }
 
 export default defineComponent({
@@ -25,11 +35,19 @@ export default defineComponent({
       cityList.value.map((d: City) => {
         return {
           label: d.cityName,
-          link: `/${d.cityCode}`,
+          path: `/${d.cityCode}`,
         }
       })
     )
-    console.log(items)
+
+    const change = (): void => {
+      EventBus.$emit(TOGGLE_EVENT)
+    }
+
+    return {
+      items,
+      change,
+    }
   },
 })
 </script>
