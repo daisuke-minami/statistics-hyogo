d775f28bff52d344291d2ff8925c7ef98bb6d2f8
daisuke-minami <daisuke.minami.1979@gmail.com>
2021-12-07 20:13:19 +0900

    card追加

---
+++ components/index/landweather/CardsClimatePrefecture.vue
@@ -0,0 +1,36 @@
+<template>
+  <cards-lazy-row :rows="rows" />
+</template>
+
+<script lang="ts">
+import CardsLazyRow from '@/components/index/_shared/CardsLazyRow.vue'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+// 快晴日数
+const NaturePark = () => {
+  return import(
+    '~/components/index/landweather/cards/climate/sunnyDaysPrefecture.vue'
+  )
+}
+
+// 自然環境保全地域面積
+// const NaturalEnvironmentConservationArea = () => {
+//   return import(
+//     '~/components/index/landweather/cards/park/naturalEnvironmentConservationAreaPrefecture.vue'
+//   )
+// }
+
+export default defineComponent({
+  components: {
+    CardsLazyRow,
+  },
+  setup() {
+    // Card
+    const rows = ref([[NaturePark]])
+
+    return {
+      rows,
+    }
+  },
+})
+</script>
---
+++ components/index/landweather/cards/park/naturalEnvironmentConservationAreaPrefecture.vue
@@ -0,0 +1,71 @@
+<template>
+  <lazy-component :is="chartComponent" v-bind="props" />
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref } from '@nuxtjs/composition-api'
+import {
+  CardTitle,
+  EstatParams,
+  EstatSeries,
+  EstatTimes,
+} from '~/utils/formatEstat'
+
+export default defineComponent({
+  setup() {
+    // Chartコンポーネントの設定
+    const chartComponent = ref<string>('estat-column-card')
+
+    // cardタイトル
+    const cardTitle = reactive<CardTitle>({
+      title: '自然環境保全地域面積',
+      titleId: 'natural-environment-conservation-area',
+    })
+
+    // estatParams cdAreaはestatコンポーネントで設定
+    const estatParams = reactive<EstatParams>({
+      statsDataId: '0000010102',
+      cdCat01: ['B1108'],
+    })
+    const estatSeries = reactive<EstatSeries[]>([
+      {
+        id: 'cat01',
+        code: 'B1108',
+        name: '自然環境保全地域面積',
+      },
+      {
+        id: 'cat01',
+        code: 'B2105',
+        name: '国定公園面積',
+      },
+      {
+        id: 'cat01',
+        code: 'B2104',
+        name: '国立公園面積',
+      },
+      {
+        id: 'cat01',
+        code: 'B2103',
+        name: '都道府県立自然公園面積',
+      },
+    ])
+    const estatLatestYear = reactive<EstatTimes>({
+      yearInt: 2019,
+      yearStr: '2019100000',
+      yearName: '2019年',
+    })
+    const estatAnnotation = reactive<string[]>([])
+
+    return {
+      chartComponent,
+      props: {
+        cardTitle,
+        estatParams,
+        estatSeries,
+        estatLatestYear,
+        estatAnnotation,
+      },
+    }
+  },
+})
+</script>
