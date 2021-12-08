3d6c55523bc73d889c8c64fd25ef6f35c3c176ed
daisuke-minami <daisuke.minami.1979@gmail.com>
2021-12-08 10:41:21 +0900

    外国人を追加

---
+++ components/index/international/CardsForeignerPrefecture.vue
@@ -0,0 +1,35 @@
+<template>
+  <cards-lazy-row :rows="rows" />
+</template>
+
+<script lang="ts">
+import CardsLazyRow from '@/components/index/_shared/CardsLazyRow.vue'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+// 外国人人口（男女別）
+const ForeignPopulation = () => {
+  return import(
+    '~/components/index/international/cards/foreigner/foreignPopulationPrefecture.vue'
+  )
+}
+// RankChart
+// const PopulationPrefectureRank = () => {
+//   return import(
+//     '@/components/index/population/cards/populationPrefectureRank.vue'
+//   )
+// }
+
+export default defineComponent({
+  components: {
+    CardsLazyRow,
+  },
+  setup() {
+    // Card
+    const rows = ref([[ForeignPopulation]])
+
+    return {
+      rows,
+    }
+  },
+})
+</script>
