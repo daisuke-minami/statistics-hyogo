543351c4c390fc28827f327cdd45bd8e0d10de16
daisuke-minami <daisuke.minami.1979@gmail.com>
2021-12-08 11:52:44 +0900

    card追加

---
+++ components/index/construction/CardsConstructionPrefecture.vue
@@ -0,0 +1,35 @@
+<template>
+  <cards-lazy-row :rows="rows" />
+</template>
+
+<script lang="ts">
+import CardsLazyRow from '@/components/index/_shared/CardsLazyRow.vue'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+// 建設業者数
+const Builders = () => {
+  return import(
+    '~/components/index/construction/cards/construction/buildersPrefecture.vue'
+  )
+}
+// RankChart
+// const PopulationPrefectureRank = () => {
+//   return import(
+//     '@/components/index/population/cards/populationPrefectureRank.vue'
+//   )
+// }
+
+export default defineComponent({
+  components: {
+    CardsLazyRow,
+  },
+  setup() {
+    // Card
+    const rows = ref([[Builders]])
+
+    return {
+      rows,
+    }
+  },
+})
+</script>
