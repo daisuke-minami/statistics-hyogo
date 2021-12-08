b6c9081f77796bb4c83487257dd41f5db356c733
daisuke-minami <daisuke.minami.1979@gmail.com>
2021-12-08 10:52:53 +0900

    企業・家計・経済を追加

---
+++ components/index/economy/cards/gross-domestic-product/industryClassPrefecture.vue
@@ -0,0 +1,71 @@
+<template>
+  <lazy-component :is="chartComponent" v-bind="props" />
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref } from '@nuxtjs/composition-api'
+import {
+  CardTitle,
+  EstatParams,
+  EstatSeries,
+  EstatTimes,
+} from '~/utils/formatEstat'
+
+export default defineComponent({
+  setup() {
+    // Chartコンポーネントの設定
+    const chartComponent = ref<string>('estat-column-card-all-break')
+
+    // cardタイトル
+    const cardTitle = reactive<CardTitle>({
+      title: '県内総生産(平成23年基準)',
+      titleId: 'industry-class',
+    })
+
+    // estatParams cdAreaはestatコンポーネントで設定
+    const estatParams = reactive<EstatParams>({
+      statsDataId: '0000010103',
+      cdCat01: ['C1111', 'C1115', 'C1116', 'C1117'],
+    })
+    const estatSeries = reactive<EstatSeries[]>([
+      {
+        id: 'cat01',
+        code: 'C1111',
+        name: '県内装生産額',
+      },
+      {
+        id: 'cat01',
+        code: 'C1115',
+        name: '第１次産業',
+      },
+      {
+        id: 'cat01',
+        code: 'C1116',
+        name: '第２次産業',
+      },
+      {
+        id: 'cat01',
+        code: 'C1117',
+        name: '第３次産業',
+      },
+    ])
+    const estatLatestYear = reactive<EstatTimes>({
+      yearInt: 2019,
+      yearStr: '2019100000',
+      yearName: '2019年',
+    })
+    const estatAnnotation = reactive<string[]>([])
+
+    return {
+      chartComponent,
+      props: {
+        cardTitle,
+        estatParams,
+        estatSeries,
+        estatLatestYear,
+        estatAnnotation,
+      },
+    }
+  },
+})
+</script>
