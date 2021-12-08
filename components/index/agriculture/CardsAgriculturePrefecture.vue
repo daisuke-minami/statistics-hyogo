f0dea278ce24974045011da62f5f71c15daa088c
daisuke-minami <daisuke.minami.1979@gmail.com>
2021-12-08 11:20:30 +0900

    card追加

---
+++ components/index/agriculture/CardsAgriculturePrefecture.vue
@@ -0,0 +1,35 @@
+<template>
+  <cards-lazy-row :rows="rows" />
+</template>
+
+<script lang="ts">
+import CardsLazyRow from '@/components/index/_shared/CardsLazyRow.vue'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+// 農家数
+const Farmers = () => {
+  return import(
+    '~/components/index/agriculture/cards/agriculture/farmersPrefecture.vue'
+  )
+}
+// RankChart
+// const PopulationPrefectureRank = () => {
+//   return import(
+//     '@/components/index/population/cards/populationPrefectureRank.vue'
+//   )
+// }
+
+export default defineComponent({
+  components: {
+    CardsLazyRow,
+  },
+  setup() {
+    // Card
+    const rows = ref([[Farmers]])
+
+    return {
+      rows,
+    }
+  },
+})
+</script>
---
+++ components/index/miningindustry/CardsManufacturingIndustryPrefecture.vue
@@ -0,0 +1,35 @@
+<template>
+  <cards-lazy-row :rows="rows" />
+</template>
+
+<script lang="ts">
+import CardsLazyRow from '@/components/index/_shared/CardsLazyRow.vue'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+// 製造品出荷額等
+const ProductShipmentValue = () => {
+  return import(
+    '~/components/index/miningindustry/cards/manufacturing-industry/productShipmentValuePrefecture.vue'
+  )
+}
+// RankChart
+// const PopulationPrefectureRank = () => {
+//   return import(
+//     '@/components/index/population/cards/populationPrefectureRank.vue'
+//   )
+// }
+
+export default defineComponent({
+  components: {
+    CardsLazyRow,
+  },
+  setup() {
+    // Card
+    const rows = ref([[ProductShipmentValue]])
+
+    return {
+      rows,
+    }
+  },
+})
+</script>
