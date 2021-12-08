77841d3f07192bbf7a6b0f81d2d6a907e1d6db5f
daisuke-minami <daisuke.minami.1979@gmail.com>
2021-12-08 13:59:49 +0900

    card追加

---
+++ components/index/administrativefinancial/CardsStaffPrefecture.vue
@@ -0,0 +1,35 @@
+<template>
+  <cards-lazy-row :rows="rows" />
+</template>
+
+<script lang="ts">
+import CardsLazyRow from '@/components/index/_shared/CardsLazyRow.vue'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+// 一般行政部門職員数
+const AdministrativeStaff = () => {
+  return import(
+    '~/components/index/administrativefinancial/cards/staff/administrativeStaffPrefecture.vue'
+  )
+}
+// RankChart
+// const PopulationPrefectureRank = () => {
+//   return import(
+//     '@/components/index/population/cards/populationPrefectureRank.vue'
+//   )
+// }
+
+export default defineComponent({
+  components: {
+    CardsLazyRow,
+  },
+  setup() {
+    // Card
+    const rows = ref([[AdministrativeStaff]])
+
+    return {
+      rows,
+    }
+  },
+})
+</script>
