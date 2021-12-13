3219d570ad25355588d6bf19614d11bd9a621779
daisuke-minami <daisuke.minami.1979@gmail.com>
2021-12-13 20:48:40 +0900

    card追加

---
+++ components/index/safetyenvironment/CardsEnvironmentPrefecture.vue
@@ -0,0 +1,29 @@
+<template>
+  <cards-lazy-row :rows="rows" />
+</template>
+
+<script lang="ts">
+import CardsLazyRow from '@/components/index/_shared/CardsLazyRow.vue'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+// 温室効果ガス算定排出量
+const CalculatedGreenhouseGasEmissions = () => {
+  return import(
+    '~/components/index/safetyenvironment/cards/environment/calculatedGreenhouseGasEmissionsPrefecture.vue'
+  )
+}
+
+export default defineComponent({
+  components: {
+    CardsLazyRow,
+  },
+  setup() {
+    // Card
+    const rows = ref([[CalculatedGreenhouseGasEmissions]])
+
+    return {
+      rows,
+    }
+  },
+})
+</script>
