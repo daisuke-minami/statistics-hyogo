cfa345c8f24fa327c5d988d701536232f4f376a9
daisuke-minami <daisuke.minami.1979@gmail.com>
2021-12-18 20:25:45 +0900

    card追加

---
+++ components/index/educationsports/CardsKindergartenCity.vue
@@ -0,0 +1,29 @@
+<template>
+  <cards-lazy-row :rows="rows" />
+</template>
+
+<script lang="ts">
+import CardsLazyRow from '@/components/index/_shared/CardsLazyRow.vue'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+// 幼稚園数
+const Kindergartens = () => {
+  return import(
+    '~/components/index/educationsports/cards/kindergarten/kindergartensCity.vue'
+  )
+}
+
+export default defineComponent({
+  components: {
+    CardsLazyRow,
+  },
+  setup() {
+    // Card
+    const rows = ref([[Kindergartens]])
+
+    return {
+      rows,
+    }
+  },
+})
+</script>
---
+++ components/index/educationsports/CardsPrimarySchoolsCity.vue
@@ -0,0 +1,29 @@
+<template>
+  <cards-lazy-row :rows="rows" />
+</template>
+
+<script lang="ts">
+import CardsLazyRow from '@/components/index/_shared/CardsLazyRow.vue'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+// 小学校数
+const PrimarySchools = () => {
+  return import(
+    '~/components/index/educationsports/cards/primary-schools/primarySchoolsCity.vue'
+  )
+}
+
+export default defineComponent({
+  components: {
+    CardsLazyRow,
+  },
+  setup() {
+    // Card
+    const rows = ref([[PrimarySchools]])
+
+    return {
+      rows,
+    }
+  },
+})
+</script>
