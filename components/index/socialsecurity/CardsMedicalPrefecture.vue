93566fc89616ea8f5dd6f2d3b4fb79f496d507ab
daisuke-minami <daisuke.minami.1979@gmail.com>
2021-12-18 23:34:28 +0900

    card追加

--- components/index/socialsecurity/CardsMedicalPrefecture.vue
+++ components/index/socialsecurity/CardsMedicalPrefecture.vue
@@ -13,13 +13,37 @@ const NationalMedicalExpenses = () => {
   )
 }
 
+// 老人福祉施設数
+const WelfareFacilitiesElderly = () => {
+  return import(
+    '~/components/index/socialsecurity/cards/medical/welfareFacilitiesElderlyPrefecture.vue'
+  )
+}
+
+// 国民健康保険被保険者数
+const NationalHealthInsurance = () => {
+  return import(
+    '~/components/index/socialsecurity/cards/medical/nationalHealthInsurancePrefecture.vue'
+  )
+}
+
+// 国民健康保険給付金額
+const NationalHealthInsuranceBenefitAmount = () => {
+  return import(
+    '~/components/index/socialsecurity/cards/medical/nationalHealthInsuranceBenefitAmountPrefecture.vue'
+  )
+}
+
 export default defineComponent({
   components: {
     CardsLazyRow,
   },
   setup() {
     // Card
-    const rows = ref([[NationalMedicalExpenses]])
+    const rows = ref([
+      [NationalMedicalExpenses, WelfareFacilitiesElderly],
+      [NationalHealthInsurance, NationalHealthInsuranceBenefitAmount],
+    ])
 
     return {
       rows,
