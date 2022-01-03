0b06aebac1fdc73164edce8c37352b9d05c50ff0
daisuke-minami <daisuke.minami.1979@gmail.com>
2022-01-03 17:29:00 +0900

    test

--- ui-test/ogp_screenshot.py
+++ ui-test/ogp_screenshot.py
@@ -3,9 +3,9 @@ import time
 import json
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 
 if not os.path.exists("ogp"):
     os.mkdir("ogp")
@@ -26,11 +26,11 @@ for value in card_data:
     driver.get(
         "http://localhost:8000{}?ogp=true".format(path)
     )
-    elem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "DataView-Header"))
+    # elem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "DataView-Header"))
 
     path = path.replace("/", "_")
-    if ('heatmap' in path):
-        time.sleep(20)
+    # if ('heatmap' in path):
+    time.sleep(20)
     driver.save_screenshot(
         "ogp/{}.png".format(path)
     )
