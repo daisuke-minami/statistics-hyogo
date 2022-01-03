77cb4fde5905ca69c4e65867b27bb448f1e04892
daisuke-minami <daisuke.minami.1979@gmail.com>
2022-01-03 20:14:38 +0900

    ogp_screenshot.pyを修正

--- ui-test/ogp_screenshot.py
+++ ui-test/ogp_screenshot.py
@@ -18,21 +18,18 @@ options.add_argument("--headless")
 options.add_argument("--hide-scrollbars")
 
 driver = webdriver.Chrome(options=options)
-# driver.set_page_load_timeout(10)
 
 for value in card_data:
     driver.set_window_size(*(value['ogpWidth'], value['ogpHeight']))
     path = value['path']
-    # driver.get(
-    #     "http://localhost:8000{}?ogp=true".format(path)
-    # )
+
     driver.get(
         "https://statistics-hyogo.com{}?ogp=true".format(path)
     )
-    # elem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "DataView-Header"))
+
 
     path = path.replace("/", "_")
-    # if ('heatmap' in path):
+
     time.sleep(20)
     driver.save_screenshot(
         "ogp/{}.png".format(path)
