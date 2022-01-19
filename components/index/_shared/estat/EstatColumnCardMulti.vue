540c5a04cd7018bc0f696ba16787b7223c670b8e
daisuke-minami <daisuke.minami.1979@gmail.com>
2022-01-19 17:16:50 +0900

    estat-column-card-multiを修正

--- components/index/_shared/estat/EstatColumnCardMulti.vue
+++ components/index/_shared/estat/EstatColumnCardMulti.vue
@@ -1,190 +1,104 @@
 <template>
-  <v-col cols="12" md="6" class="DataCard">
-    <client-only>
-      <template>
-        <v-card :loading="$fetchState.pending">
-          <p v-if="$fetchState.pending" />
-          <data-view v-else :title="title" :route="routingPath">
-            <h4 :id="titleId" class="visually-hidden">
-              {{ title }}
-            </h4>
+  <data-view :title="title" :route="path">
+    <h4 :id="titleId" class="visually-hidden">
+      {{ title }}
+    </h4>
 
-            <lazy-component
-              :is="chartComponent"
-              v-show="canvas"
-              :display-data="displayData"
-            />
+    <!-- <template v-slot:infoPanel>
+      <data-view-data-set-panel :display-info="displayInfo" />
+    </template> -->
 
-            <template v-slot:description>
-              <p>最終更新日：{{ lastUpdate }}</p>
-              <slot name="description" />
-            </template>
+    <lazy-component
+      :is="chartComponent"
+      v-show="canvas"
+      :display-data="displayData"
+    />
 
-            <template v-slot:additionalDescription>
-              <span>（注）</span>
-              <ul>
-                <li v-for="item in additionalDescription" :key="item">
-                  {{ item }}
-                </li>
-              </ul>
-              <slot name="additionalDescription" />
-            </template>
+    <template v-slot:description>
+      <p>最終更新日:{{ lastUpdate }}</p>
+      <slot name="description" />
+    </template>
 
-            <template v-slot:dataTable>
-              <client-only>
-                <data-view-table :headers="tableHeader" :items="tableData" />
-              </client-only>
-            </template>
+    <template v-slot:additionalDescription>
+      <span>（注）</span>
+      <ul>
+        <li v-for="item in additionalDescription" :key="item">
+          {{ item }}
+        </li>
+      </ul>
+      <slot name="additionalDescription" />
+    </template>
 
-            <template v-slot:footer>
-              <app-link :to="source.estatUrl">
-                {{ source.estatName }}
-              </app-link>
-            </template>
-          </data-view>
-        </v-card>
-      </template>
-    </client-only>
-  </v-col>
+    <template v-slot:dataTable>
+      <client-only>
+        <data-view-table :headers="tableHeader" :items="tableData" />
+      </client-only>
+    </template>
+
+    <template v-slot:footer>
+      <app-link :to="source.estatUrl">
+        {{ source.estatName }}
+      </app-link>
+    </template>
+  </data-view>
 </template>
 
 <script lang="ts">
-import {
-  defineComponent,
-  ref,
-  computed,
-  useFetch,
-  PropType,
-  inject,
-} from '@nuxtjs/composition-api'
-import {
-  EstatParams,
-  EstatSeries,
-  EstatTimes,
-  EstatResponse,
-  EstatSource,
-  formatTimeChart,
-  CardTitle,
-  formatAdditionalDescription,
-} from '@/utils/formatEstat'
-import { StateType, StateKey } from '@/composition/useState'
+import { defineComponent, computed } from '@nuxtjs/composition-api'
+import { useEstatTimeChart } from '@/composition/useEstatTimeChart'
 
 export default defineComponent({
   props: {
-    cardTitle: {
-      type: Object as PropType<CardTitle>,
-      required: true,
-    },
-    estatParams: {
-      type: Object as PropType<EstatParams>,
-      required: true,
-    },
-    estatSeries: {
-      type: Array as PropType<EstatSeries[]>,
-      required: true,
-    },
-    estatLatestYear: {
-      type: Object as PropType<EstatTimes>,
-      required: true,
-    },
-    estatAnnotation: {
-      type: Array as PropType<string[]>,
+    estatState: {
+      type: Object,
       required: true,
     },
   },
-  setup(props, context) {
+  setup(props) {
     // canvas
-    const canvas = ref<boolean>(true)
+    const canvas = true
 
-    // inject
-    const State: StateType = inject(StateKey)
-    const code = State.code.value
-    const govType = State.govType.value
-    const selectedPref = State.selectedPref.value
-    const selectedCity = State.selectedCity.value
-
-    /* eslint-disable no-console */
-    console.log({ code, govType, selectedPref, selectedCity })
-
-    // card情報の設定
-    const title = computed((): string => {
-      const name: string =
-        govType === 'prefecture' ? selectedPref.prefName : selectedCity.cityName
-      return `${name}の${props.cardTitle.title}`
-    })
-    const titleId = computed((): string => {
-      return `${props.cardTitle.titleId}`
-    })
-    const routingPath = computed((): string => {
-      return `${State.routingPath.value}/${titleId.value}/`
-    })
-
-    // eStat-APIからデータを取得
-    const estatResponse = ref<EstatResponse>({})
-    const { fetch } = useFetch(async () => {
-      const params = Object.assign({}, props.estatParams)
-      params.cdArea = code
-      const { data: res } = await context.root.$estat.get('getStatsData', {
-        params,
-      })
-      estatResponse.value = res
-    })
-    fetch()
-
-    // データの整形
-    const series: EstatSeries[] = props.estatSeries
-    const formatData = computed(() => {
-      return formatTimeChart(estatResponse.value, series)
-    })
+    const {
+      title,
+      titleId,
+      path,
+      lastUpdate,
+      chartData,
+      tableHeader,
+      tableData,
+      source,
+      additionalDescription,
+    } = useEstatTimeChart(props.estatState)
 
     // chartの種類を設定
-    const chartComponent = ref<string>('column-chart')
+    const chartComponent = 'column-chart'
 
     // displayData
     const displayData = computed(() => {
-      return formatData.value.chartData
+      return chartData.value
     })
-
-    // テーブルの設定
-    const tableHeader = computed(() => {
-      return formatData.value.tableHeader
-    })
-    const tableData = computed(() => {
-      return formatData.value.tableData
-    })
-
-    // 出典
-    const source = computed((): EstatSource => {
-      return formatData.value.source
-    })
-
-    const lastUpdate = computed((): string => {
-      if (process.browser) {
-        const day = new Date(document.lastModified)
-        return `${day.getFullYear()}年${day.getMonth() + 1}月${day.getDate()}日`
-      } else {
-        return ''
-      }
-    })
-
-    // 注釈
-    const additionalDescription = computed((): string[] => {
-      return formatAdditionalDescription(props.estatAnnotation).timeChart
-    })
+    // const displayInfo = computed(() => {
+    //   const d: EstatSeries = chartData.value[0]
+    //   const l: number = d.data.length
+    //   return {
+    //     lText: d.data[l - 1].y.toLocaleString(),
+    //     sText: d.data[l - 1].x + '年の' + d.name,
+    //     unit: d.data[l - 1].unit,
+    //   }
+    // })
 
     return {
       title,
       titleId,
-      routingPath,
+      path,
       lastUpdate,
-      // allbreak,
+      // displayInfo,
       displayData,
       additionalDescription,
       source,
       tableHeader,
       tableData,
       canvas,
-      // displayInfo,
       chartComponent,
     }
   },
--- components/index/population/cards/bitrh/birthMotherAgePrefecture.vue
+++ components/index/population/cards/bitrh/birthMotherAgePrefecture.vue
@@ -1,104 +1,117 @@
 <template>
-  <lazy-component :is="chartComponent" v-bind="props" />
+  <v-col cols="12" md="6" class="DataCard">
+    <client-only>
+      <template>
+        <v-card :loading="$fetchState.pending">
+          <p v-if="$fetchState.pending" />
+          <lazy-component :is="cardComponent" v-else v-bind="props" />
+        </v-card>
+      </template>
+    </client-only>
+  </v-col>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref } from '@nuxtjs/composition-api'
 import {
-  CardTitle,
-  EstatParams,
-  EstatSeries,
-  EstatTimes,
-} from '~/utils/formatEstat'
+  defineComponent,
+  reactive,
+  useFetch,
+  useContext,
+  useRoute,
+} from '@nuxtjs/composition-api'
+import { useEstatApi } from '@/composition/useEstatApi'
+import { EstatState } from '@/types/estat'
 
 export default defineComponent({
   setup() {
-    // Chartコンポーネントの設定
-    const chartComponent = ref<string>('estat-column-card-multi')
+    // cardコンポーネントの設定
+    const cardComponent = 'estat-column-card-multi'
 
-    // cardタイトル
-    const cardTitle = reactive<CardTitle>({
+    // State
+    const estatState = reactive<EstatState>({
       title: '出生数（母親の年齢別）',
       titleId: 'birth-mother-age',
+      params: {
+        statsDataId: '0000010101',
+        cdCat01: [
+          'A410201',
+          'A410202',
+          'A410203',
+          'A410204',
+          'A410205',
+          'A410206',
+          'A410207',
+          'A410208',
+          'A410209',
+        ],
+      },
+      series: [
+        {
+          id: 'cat01',
+          code: 'A410201',
+          name: '母親の年齢15歳未満',
+        },
+        {
+          id: 'cat01',
+          code: 'A410202',
+          name: '母親の年齢15〜19歳',
+        },
+        {
+          id: 'cat01',
+          code: 'A410203',
+          name: '母親の年齢20〜24歳',
+        },
+        {
+          id: 'cat01',
+          code: 'A410204',
+          name: '母親の年齢25〜29歳',
+        },
+        {
+          id: 'cat01',
+          code: 'A410205',
+          name: '母親の年齢30〜34歳',
+        },
+        {
+          id: 'cat01',
+          code: 'A410206',
+          name: '母親の年齢35〜39歳',
+        },
+        {
+          id: 'cat01',
+          code: 'A410207',
+          name: '母親の年齢40〜44歳',
+        },
+        {
+          id: 'cat01',
+          code: 'A410208',
+          name: '母親の年齢45〜49歳',
+        },
+        {
+          id: 'cat01',
+          code: 'A410209',
+          name: '母親の年齢50歳以上',
+        },
+      ],
+      annotation: [],
+      response: {},
     })
 
-    // estatParams cdAreaはestatコンポーネントで設定
-    const estatParams = reactive<EstatParams>({
-      statsDataId: '0000010101',
-      cdCat01: [
-        'A410201',
-        'A410202',
-        'A410203',
-        'A410204',
-        'A410205',
-        'A410206',
-        'A410207',
-        'A410208',
-        'A410209',
-      ],
+    // routeパラメータの取得
+    const { code } = useRoute().value.params
+
+    // eStat-APIからデータを取得
+    const { $axios } = useContext()
+    const { fetch } = useFetch(async () => {
+      const params = Object.assign({}, estatState.params)
+      params.cdArea = code
+      estatState.response = await useEstatApi($axios, params).getData()
     })
-    const estatSeries = reactive<EstatSeries[]>([
-      {
-        id: 'cat01',
-        code: 'A410201',
-        name: '母親の年齢15歳未満',
-      },
-      {
-        id: 'cat01',
-        code: 'A410202',
-        name: '母親の年齢15〜19歳',
-      },
-      {
-        id: 'cat01',
-        code: 'A410203',
-        name: '母親の年齢20〜24歳',
-      },
-      {
-        id: 'cat01',
-        code: 'A410204',
-        name: '母親の年齢25〜29歳',
-      },
-      {
-        id: 'cat01',
-        code: 'A410205',
-        name: '母親の年齢30〜34歳',
-      },
-      {
-        id: 'cat01',
-        code: 'A410206',
-        name: '母親の年齢35〜39歳',
-      },
-      {
-        id: 'cat01',
-        code: 'A410207',
-        name: '母親の年齢40〜44歳',
-      },
-      {
-        id: 'cat01',
-        code: 'A410208',
-        name: '母親の年齢45〜49歳',
-      },
-      {
-        id: 'cat01',
-        code: 'A410209',
-        name: '母親の年齢50歳以上',
-      },
-    ])
-    const estatLatestYear = reactive<EstatTimes>({
-      yearInt: 2019,
-      yearStr: '2019100000',
-      yearName: '2019年',
-    })
-    const estatAnnotation = reactive<string[]>([])
+    fetch()
 
     return {
-      chartComponent,
+      cardComponent,
       props: {
-        cardTitle,
-        estatParams,
-        estatSeries,
-        estatLatestYear,
-        estatAnnotation,
+        estatState,
       },
     }
   },
